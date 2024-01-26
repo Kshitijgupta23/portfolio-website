ebdb5efa121306aba0007169935a75eaf2b6a3ea
Kshitij Gupta <kshitij.gupta2301@gmail.com>
2024-01-26 19:49:07 +0530

    Edited link of project

--- src/constants/index.js
+++ src/constants/index.js
@@ -208,7 +208,7 @@ import {
         },
       ],
       image: zerohunger,
-      source_code_link: "https://github.com/Kshitijgupta23/ZeroHunger_Sparks",
+      source_code_link: "https://zero-hunger-sparks.vercel.app/",
       url: "https://kshitijgupta23.github.io/ZeroHunger_Sparks/",
     },
     {
